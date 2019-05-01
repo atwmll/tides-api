ab48959de4460c9e862080059c1add7e84cbaff1
atwmll <eclidu@gmail.com>
2019-04-30 21:56:22 -0400

    Updated API and changed API idea

--- script.js
+++ script.js
@@ -1,17 +1,43 @@
-"use strict"
-window.onload = function() {
-  alert("Work-in-progress! Location look-up to be added later...");
-  var station = prompt("Enter 7-digit Station Number: ");
-  if (station === null) {
-      alert("No data! Click 'OK' to reload..."); //break out of the function early
-      window.location.reload();
-    } else if (station.toString().length < 7) {
-      alert("Invalid number, press 'OK' to reload page.");
-      window.location.reload();
-    } else if (station.toString().length > 7) {
-      alert("Invalid number, press 'OK' to reload page.");
-      window.location.reload();
-    }
-  var number = station;
-  document.getElementById('station').innerHTML = number;
-};
+var zipcode = prompt(`Enter zipcode:`);
+
+var url = new URL('https://api.openweathermap.org/data/2.5/weather?zip=NULL&appid=c331effff19eabf7638fb78114be2649&units=Imperial');
+
+var query_string = url.search;
+
+var search_params = new URLSearchParams(query_string);
+
+search_params.set('zip', zipcode);
+
+url.search = search_params.toString();
+
+var new_url = url.toString();
+
+var settings = {
+  "async": true,
+  "crossDomain": true,
+  "url": new_url,
+  "method": "GET"
+}
+
+$.ajax(settings).done(function (response) {
+
+  // Location
+  var loc = response.name;
+  $("#currentLocation").append(loc);
+
+  // Temperature
+  var temp = response.main.temp;
+  $("#currentTemp").append(temp);
+
+  // Weather
+  var weather = response.weather[0].main;
+  $("#currentWeather").append(weather);
+
+  // Weather description
+  var desc = response.weather[0].description;
+  $("#weatherDesc").append(desc);
+
+  // Wind
+  var wind = response.wind.speed;
+  $("#windSpeed").append(wind);
+});
